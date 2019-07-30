cccc58f44d529e98c99b05fcc9bc7802ffdc9eeb
Leonardo <leonardolonghi64@gmail.com>
2019-07-31 00:47:28 +0200

    navbar social sharing icons

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,47 @@
 <template>
-  <div id="app">
-    <div id="nav">
-    </div>
-    <router-view/>
+  <v-app>
+    <v-app-bar app light>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>title</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <social-sharing url="variabile"
+                      title="da config"
+                      description="da config"
+                      quote="da config"
+                      hashtags="da config"
+                      twitter-user="da config"
+                      inline-template>
+  <div>
+      <network network="facebook">
+        <i class="fab fa-facebook"></i>
+      </network>
+      <network network="linkedin">
+        <i class="fab fa-linkedin"></i>
+      </network>
+      <network network="twitter">
+        <i class="fab fa-twitter"></i>
+      </network>
   </div>
+</social-sharing>
+    </v-app-bar>
+
+    <v-content>
+      <router-view />
+    </v-content>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<script>
+var socialSharing = require("vue-social-sharing");
+export default {
+  name: "App",
+  components:{
+    socialSharing
+  },
+  
+  data: () => ({
+    //
+  })
+};
+</script>

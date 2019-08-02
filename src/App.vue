474d3cc808ef6a607827468d3193a145b36ad36e
Leonardo <leonardolonghi64@gmail.com>
2019-08-02 05:01:06 +0200

    menu and footer

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,85 @@
 <template>
   <v-app>
+    <!-- TOOLBAR -->
     <v-app-bar app light>
-      <!-- <VMenu :nudge-width="100">
-        <VToolbarSideIcon slot="activator" />
-        <VList>
-          <VListTile
-            value="true" v-for="(item, i) in items"
-            :key="i"
-          >
-            <VListTileAction>
-              <VIcon v-html="item.icon" />
-            </VListTileAction>
-            <VListTileContent>
-              <VListTileTitle>
-                <RouterLink v-if="!item.external" :to="item.path" v-text="item.title" />
-                <a v-if="item.external" target="_blank" rel="noopener noreferrer" :href="item.path">{{item.title}}</a>
-                 <v-icon v-if="item.external" size="10px" class="external-link">launch</v-icon>
-              </VListTileTitle>
-            </VListTileContent>
-          </VListTile>
-        </VList>
-      </VMenu> -->
-      <v-toolbar-title class="headline text-uppercase">
-        <span>title</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-     
-    </v-app-bar>
+		<!-- NAVIGATION MENU -->
+		<v-menu>
+			<template v-slot:activator="{ on: menu }">
+				<v-icon v-on="{ ...menu }">fas fa-list</v-icon>
+			</template>
+			<v-list >
+				<v-list-item-group v-model="item" color="primary">
+				<v-list-item v-for="(item, i) in items" :key="i" >
+					<v-list-item-icon>
+					<v-icon v-text="item.icon"></v-icon>
+					</v-list-item-icon>
+					<v-list-item-content>
+					<v-list-item-title v-text="item.text"></v-list-item-title>
+					</v-list-item-content>
+				</v-list-item>
+				</v-list-item-group>
+      		</v-list>
+      	</v-menu>
+		<!--TITLE-->
+		<v-toolbar-title class="headline text-uppercase"><span>title</span></v-toolbar-title>
+		<!--SPACER-->
+		<v-spacer></v-spacer>
+		<!-- SOCIAL -->
+		<v-dialog v-model="dialog" width="500" >
+			<template v-slot:activator="{ on }">
+				<v-icon v-on="on">fas fa-share-alt</v-icon>
+			</template>
 
+			<v-card>
+				<v-card-title class="headline grey lighten-2" primary-title>
+					Condividi
+				</v-card-title>
+				<v-card-text>
+					social buttons will appare here
+				</v-card-text>
+			</v-card>
+      	</v-dialog>
+		<!-- OPTION -->
+		<v-menu>
+			<template v-slot:activator="{ on: menu }">
+				<v-icon v-on="{ ...menu }">fas fa-list</v-icon>
+			</template>
+			<v-list >
+				<v-list-item-group v-model="item" color="primary">
+				<v-list-item v-for="(item, i) in items" :key="i" >
+					<v-list-item-icon>
+					<v-icon v-text="item.icon"></v-icon>
+					</v-list-item-icon>
+					<v-list-item-content>
+					<v-list-item-title v-text="item.text"></v-list-item-title>
+					</v-list-item-content>
+				</v-list-item>
+				</v-list-item-group>
+      		</v-list>
+      	</v-menu>
+    </v-app-bar>
+    <!-- CONTENT -->
     <v-content>
       <router-view />
     </v-content>
+    <!-- FOOTER -->
   </v-app>
 </template>
 
 <script>
+import {bgoStore, fetcher, ns} from "bgo.js"
 export default {
   name: "App",
-  components:{
-  },
-  
+  components: {},
+
   data: () => ({
-    //
-  })
+    //Tutte le variabili da inserire nel template saranno messe qui
+  }),
+  mounted(){
+	  fatchData();
+  }
 };
+function fatchData(){
+	dgoS
+}
 </script>

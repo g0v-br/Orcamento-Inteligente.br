6601ee060a74e62d17f6dc3765528320e0923d2d
YassineOuahidi <marvindoufresn@gmail.com>
2019-09-11 18:38:25 +0200

    Totalizer Done
    data.ttl and sample.ttl changed

--- src/components/Totalizer.vue
+++ src/components/Totalizer.vue
@@ -1,52 +1,71 @@
 <template>
-  <p>{{totalString}}</p>
+  <p>{{display}}</p>
 </template>
 <script>
-import { bgoStore, fetcher, ns } from "@/models/bgo.js";
-export default {
-  props: {
-    total: {
-      type: String
-    }
-  },
-  computed: {
-    totalString() {
-      let totals = this.total.split(";");
-
-      let total0 = new Intl.NumberFormat({ maximumFractionDigits: 2 }).format(
-        totals[0]
-      );
-
-      let total1 = new Intl.NumberFormat({ maximumFractionDigits: 2 }).format(
-        totals[1]
-      );
-
-      let text = fetchData(totals[0] != totals[1]);
-      if (totals[0] == totals[1]) {
-        return text.replace("%s", total0);
-      } else {
-        let rate = totals[1] / totals[0];
-        rate = new Intl.NumberFormat(undefined, {
-          style: "percent",
-          maximumFractionDigits: 2
-        }).format(rate);
-        
-        if(parseFloat(rate)<0.01)
-          return text.replace("%s", total1).replace("%s", "Meno dello 0,01% ");
-        //replace works only with the first occourrence of %2
-        return text.replace("%s", total1).replace("%s", rate);
+  import { bgoStore, fetcher, ns } from "@/models/bgo.js";
+  export default {
+    props: {
+      total: {
+        type: [String, Number]
+      },
+      filtered: {
+        type: [String, Number]
       }
+    },
+    computed: {
+      display() {
+        let text, data;
+        let total, filtered, rate; //Numbers
+
+        total = new Intl.NumberFormat({ maximumFractionDigits: 2 }).format(
+          this.total
+        );
+        filtered = new Intl.NumberFormat({ maximumFractionDigits: 2 }).format(
+          this.filtered
+        );
+        
+        (this.filtered == this.total) ? data = fetchData(false) : data = fetchData(this.filtered);
+        
+        
+        text = data.text;
+        if (!this.filtered) 
+          text = text.replace("%s", total);
+        else {
+          rate = parseFloat(this.filtered) / parseFloat(this.total);
+
+          if(rate < data.treshold)
+            text = text.replace("%s", filtered).replace("%s", data.tresholdPrintTemplate);
+          else {
+            rate = new Intl.NumberFormat(undefined, {
+              style: "percent",
+              maximumFractionDigits: 2
+            }).format(rate);
+
+            text = text.replace("%s", filtered).replace("%s", rate);
+          }
+        }
+
+        return text;
     }
   }
 };
-function fetchData(filter_active) {
-  let totalizer = bgoStore.any(undefined, ns.rdf("type"), ns.bgo("Totalizer"));
-  return bgoStore.anyValue(
+
+function fetchData(filterActive) {
+  let totalizer, object;
+
+  object = {};
+  totalizer = bgoStore.any(undefined, ns.rdf("type"), ns.bgo("Totalizer"));
+  object["text"] = bgoStore.anyValue(
     totalizer,
-    filter_active
-      ? ns.bgo("totalFilteredPrintfTemplate")
-      : ns.bgo("totalPrintfTemplate")
-  );
+    filterActive
+    ? ns.bgo("totalFilteredPrintfTemplate")
+    : ns.bgo("totalPrintfTemplate")
+    );
+
+  object["treshold"] = parseFloat(bgoStore.anyValue(totalizer, ns.bgo("treshold")));
+  object["tresholdPrintTemplate"] = bgoStore.anyValue(totalizer, ns.bgo("tresholdPrintTemplate"));
+  
+  return object;
 }
 </script>
 

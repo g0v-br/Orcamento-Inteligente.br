13ac3af21a6766bbb19742840ddbbc564e6c57dd
YassineOuahidi <marvindoufresn@gmail.com>
2019-09-12 00:17:21 +0200

    TermsAndCond Done
    without CSS

--- src/views/TermsAndConditions.vue
+++ src/views/TermsAndConditions.vue
@@ -1,3 +1,24 @@
 <template>
-    <div>this is terms page</div>
+    <StringFormatter :string="abstract"/>
 </template>
+<script>
+import { bgoStore, fetcher, ns } from "@/models/bgo.js"; 
+import StringFormatter from "@/components/StringFormatter.vue";
+export default {
+  name: "Table",
+  components: {
+    StringFormatter
+  },
+  data() {
+    return {
+      title: {},
+      abstract: {}
+    };
+  },
+  created() {
+    let creditsView = bgoStore.any(undefined, ns.rdf("type"), ns.bgo("TermsView"));
+    this.title = bgoStore.any(creditsView, ns.bgo("title"));
+    this.abstract = bgoStore.any(creditsView, ns.bgo("abstract"));
+  }
+};
+</script>

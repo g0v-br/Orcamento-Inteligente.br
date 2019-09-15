5fced6d94ed56ff346e0214998e32fc7de3403c1
Miah Mohd <miahmohd98@gmail.com>
2019-09-15 21:46:50 +0200

    Update StringFormatter.vue

--- src/components/StringFormatter.vue
+++ src/components/StringFormatter.vue
@@ -1,95 +1,88 @@
 <template>
-  <div>
-    <span 
-    class="stringDisplayed"
-    :id="hash(string.value)"
-    ></span>
-    <v-icon
-    v-if="hasPopup"
-    @click="dialog=true"
-    medium
-    >mdi-information-outline</v-icon>
+  <div style="display:flex; align-items:center;">
+    <span class="stringDisplayed" :id="hash(string.value)"></span>
+    <v-icon class="info-icon"  v-if="hasPopup" @click="dialog=true" medium>mdi-information-outline</v-icon>
     <v-dialog v-model="dialog" max-width="290" clearable>
       <v-card>
-        <v-card-text>
-          {{popup.value}}
-        </v-card-text>
+        <v-card-text>{{popup.value}}</v-card-text>
       </v-card>
     </v-dialog>
   </div>
 </template>
 
 <script>
-  let defaultObject = {
-    value : "default",
-    datatype: {
-      value : "default"
-    }
+let defaultObject = {
+  value: "default",
+  datatype: {
+    value: "default"
   }
-  import { ns } from "@/models/bgo.js";
-  import Markdown from "markdown-it";
-  export default {
-    props: {
-      popup: {
-        type: Object,
-        default: () => {
-          return defaultObject;
-        }
-      },
-      string: {
-        type: Object,
-        default: () => {
-          return defaultObject;
-        }
+};
+import { ns } from "@/models/bgo.js";
+import Markdown from "markdown-it";
+export default {
+  props: {
+    popup: {
+      type: Object,
+      default: () => {
+        return defaultObject;
       }
     },
-    data () {
-      return {
-        dialog: false,
-        hasPopup: true
+    string: {
+      type: Object,
+      default: () => {
+        return defaultObject;
       }
-    },
-    mounted() {
-      let divDisplay = document.getElementById(this.hash(this.string.value));
-      let md = new Markdown();
+    }
+  },
+  data() {
+    return {
+      dialog: false,
+      hasPopup: true
+    };
+  },
+  mounted() {
+    let divDisplay = document.getElementById(this.hash(this.string.value));
+    let md = new Markdown();
 
-      let display;
+    let display;
 
-      switch(this.string.datatype.value) {
-        case ns.bgo("MDString").value:
+    switch (this.string.datatype.value) {
+      case ns.bgo("MDString").value:
         display = md.render(this.string.value);
         break;
-        default:
+      default:
         display = this.string.value;
         break;
-      }
-      divDisplay.innerHTML = display;
+    }
+    divDisplay.innerHTML = display;
 
-      //if it has popup
-      this.hasPopup = (this.popup == undefined || this.popup.value == defaultObject.value) ? false : true;
-    },
-    methods: {
-      hash(string) {
-        let hash = 0;
-        if (string.length == 0) {
-          return hash;
-        }
-        for (let i = 0; i < string.length; i++) {
-          var char = string.charCodeAt(i);
-          hash = ((hash<<5)-hash)+char;
-          hash = hash & hash;
-        }
+    //if it has popup
+    this.hasPopup =
+      this.popup == undefined || this.popup.value == defaultObject.value
+        ? false
+        : true;
+  },
+  methods: {
+    hash(string) {
+      let hash = 0;
+      if (string.length == 0) {
         return hash;
       }
+      for (let i = 0; i < string.length; i++) {
+        var char = string.charCodeAt(i);
+        hash = (hash << 5) - hash + char;
+        hash = hash & hash;
+      }
+      return hash;
     }
-  };
+  }
+};
 </script>
 <style type="text/css" media="screen">
-.icon {
-  display:inline-block
+.info-icon:hover {
+  color: #1976D2;
 }
-.stringDisplayed{
-  margin: 3 em
+.stringDisplayed {
+  margin-right: .5em;
 }
-
 </style>

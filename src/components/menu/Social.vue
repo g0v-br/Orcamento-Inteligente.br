ea1027c281f0f03ed833c5ba634c7ac3ede9b89c
Leonardo <leonardolonghi64@gmail.com>
2019-09-16 03:33:11 +0200

    style popup

--- src/components/menu/Social.vue
+++ src/components/menu/Social.vue
@@ -12,49 +12,76 @@
         :description="description"
         :hashtags="hashtag"
         inline-template>
-      <div >
-          <network network="email" class="g0v-social-link">
-            <i class="fas fa-envelope"></i>
-          </network>
+      <div class="container-social-link" >
+        <div class="social-link email-link">
+          <network  network="email" >
+            <i class="fa fa-envelope"></i>
+          </network></div>
+          <div class="social-link facebook-link">
           <network network="facebook">
             <i class="fab fa-facebook"></i>
           </network>
+          </div>
+          <div class="social-link line-link">
           <network network="line">
             <i class="fab fa-line"></i>
           </network>
+          </div>
+          <div class="social-link linkedin-link">
           <network network="linkedin">
             <i class="fab fa-linkedin"></i>
           </network>
+          </div>
+          <div class="social-link ok-link">
           <network network="odnoklassniki">
             <i class="fab fa-odnoklassniki"></i>
           </network>
+          </div>
+          <div class="social-link pinintrest-link">
           <network network="pinterest">
             <i class="fab fa-pinterest"></i>
           </network>
+          </div>
+          <div class="social-link reddit-link">
           <network network="reddit">
             <i class="fab fa-reddit"></i>
           </network>
+          </div>
+          <div class="social-link skype-link">
           <network network="skype">
             <i class="fab fa-skype"></i>
           </network>
+          </div>
+          <div class="social-link twitter-link">
           <network network="twitter">
             <i class="fab fa-twitter"></i>
           </network>
+          </div>
+          <div class="social-link vk-link">
           <network network="vk">
             <i class="fab fa-vk"></i>
           </network>
+          </div>
+          <div class="social-link weibo-link">
           <network network="weibo">
             <i class="fab fa-weibo"></i>
           </network>
+          </div>
+          <div class="social-link whatsapp-link">
           <network network="whatsapp">
             <i class="fab fa-whatsapp"></i>
           </network>
+          </div>
       </div>
       </SocialSharing>
       <hr>
-      <div>
-        <input type="text" readonly ref="url" :value="getUrl" />
-        <button v-on:click="copyToClipboard()">Copy url</button>
+      <div class="copy-url">
+        <div class="input-link">
+        <input  type="text" readonly ref="url" :value="getUrl" />
+        </div>
+        <div class="icon-copy">
+        <v-icon  v-on:click="copyToClipboard()">mdi-content-copy</v-icon>
+        </div>
       </div>
     </v-card>
   </v-dialog>
@@ -88,6 +115,7 @@ export default {
   },
   mounted() {
     fetchData(this);
+  console.log(this.$el);
   }
 };
 function fetchData(app){
@@ -103,15 +131,55 @@ function fetchData(app){
     app.hashtag= hashtag? (hashtag.value) : "#LODMAP2D";
 }
 </script>
-<style scoped>
-  .g0v-social-link {
-  width: 32px;
-  font-size: 20px;
-  display: inline-block;
-  text-align: center;
-  cursor: pointer;
-  color: rgba(0, 0, 0, 0.54)!important;
+<style>
+.copy-url{
+  padding: 0.5em;
+  
 }
+.icon-copy{
+  display: inline;
+};
+.input-link{
+  display: inline;
+};
+  .container-social-link{
+    padding: 0.5em;
+    height: 200px;
+  }
+
+  .social-link{
+    text-align: center;
+    font-size: 50px;
+    width: 50px;
+    display: inline-block;
+	 margin: 14px;
+    -webkit-transition: all 0.3s ease-in-out;
+	-moz-transition: all 0.3s ease-in-out;
+	-ms-transition: all 0.3s ease-in-out;
+	-o-transition: all 0.3s ease-in-out;
+	transition: all 0.3s ease-in-out;
+    
+}
+
+.social-link:hover, .social-link:active { 
+ 	-webkit-transform: scale(1.1);
+	-moz-transform: scale(1.1);
+	-ms-transform: scale(1.1);
+	-o-transform: scale(1.1);
+	transform: scale(1.1); 
+}
+.facebook-link{ color:#3C599F;}
+.line-link{color:#00c300;}
+.linkedin-link{color:#0077B5;}
+.pinintrest-link{color:#bd081c;}
+.whatsapp-link{color:#25D366;}
+.skype-link{color:#00AFF0;}
+.twitter-link{color:#55acee;}
+.vk-link{color:#4c75a3;}
+.weibo-link{color:#df2029;}
+.reddit-link{color:#ff4500;}
+.ok-link{color:#ff9900;}
+.email-link{color:	#383838;}
 </style>
 
 

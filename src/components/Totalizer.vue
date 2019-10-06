21770459758c4f9d6dc8337b30e178f254143113
YassineOuahidi <marvindoufresn@gmail.com>
2019-10-07 00:38:42 +0200

    close issue #31
    Needs to be used in the whole application. missing default params.

--- src/components/Totalizer.vue
+++ src/components/Totalizer.vue
@@ -3,7 +3,7 @@
 </template>
 <script>
 import { bgoStore, fetcher, ns } from "@/models/bgo.js";
-import { formatPercentage, formatAmount, printf } from "@/utils/utils.js";
+import { formatPercentage, formatAmount, printf, numberFormatter} from "@/utils/utils.js";
 export default {
   props: {
     total:[String, Number],
@@ -13,23 +13,20 @@ export default {
   computed: {
     display() {
       let text, data;
-      let total, filtered, rate; //Numbers
-
-      total = formatAmount(this.total, this.options.precision);
-      filtered = formatAmount(this.filtered, this.options.precision);
+      let rate;
 
       if (Math.round(this.filtered) == Math.round(this.total)) 
-        text = this.options.format.replace("%s", total);
+        text = numberFormatter(this.total, {
+          precision: this.options.precision, 
+          format: this.options.format
+        });
       else {
-        rate = parseFloat(this.filtered) / parseFloat(this.total);
-        text = this.options.filteredFormat.replace("%s", filtered);
-
-        if (Math.abs(rate) < this.options.rateFormatter.minValue)
-          text = text + this.options.rateFormatter.lessThanMinFormat;
-        else {
-          rate = formatPercentage(rate, options.rateFormatter.precision);
-          text = text + this.options.rateFormatter.format.replace("%s", rate);
-        }
+        rate = (parseFloat(this.filtered) / parseFloat(this.total)) * this.options.rateFormatter.scaleFactor;
+        text = numberFormatter(this.filtered, {
+          precision: this.options.precision, 
+          format: this.options.filteredFormat
+        });
+        text = text + numberFormatter(rate, this.options.rateFormatter);
       }
 
       return text;

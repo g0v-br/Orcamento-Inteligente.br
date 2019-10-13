04d36770eee4f657daa3e24a91b6912c2be3d32b
Yassine Ouahidi <yass.ouahidi@gmail.com>
2019-10-13 23:44:00 +0200

    BugFix

--- src/services/creditsService.js
+++ src/services/creditsService.js
@@ -1,8 +1,18 @@
-import { bgoStore, ns } from "./rdfService";
+import { store, ns } from "./rdfService";
+const domain = store.any(undefined, ns.bgo("hasCredits"));
+const creditsView = store.any(domain, ns.bgo("hasCredits"));
 
 export default function () {
 
     return {
+        getData: () => {
+            return {
 
+                title: store.any(creditsView, ns.bgo("title")),
+                abstract: store.any(creditsView, ns.bgo("abstract"))
+
+            }
+        },
+        
     }
 }
--- src/services/termsService.js
+++ src/services/termsService.js
@@ -1,8 +1,18 @@
-import { bgoStore, ns } from "./rdfService";
+import { store, ns } from "./rdfService";
+const domain = store.any(undefined, ns.bgo("hasTerms"));
+const termsView = store.any(domain, ns.bgo("hasTerms"));
 
 export default function () {
 
     return {
+        getData: () => {
+            return {
+
+                title: store.any(termsView, ns.bgo("title")),
+                abstract: store.any(termsView, ns.bgo("abstract"))
+
+            }
+        },
 
     }
 }

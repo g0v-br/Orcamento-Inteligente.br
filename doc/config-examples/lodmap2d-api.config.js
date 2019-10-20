8fbf0a901b41381aa6497206e583ed0628d96048
Configuration Manager <admin@linkeddata.center>
2019-10-20 11:08:01 +0200

    Changed config for LODMAP2d-api

--- doc/config-examples/lodmap2d-api.config.js
+++ doc/config-examples/lodmap2d-api.config.js
@@ -8,12 +8,14 @@
  *  Note that http://localhost:8000/app.ttl is always loaded (because "isLast": true is not present
  */
 (function (window) {
-	let namespace = "http://localhost:8000";
+	const LODMAP2D_DATA = "http://localhost:8000";
 	
 	window.__dereferencingRules = [
-		{ "regexp": ".*", "targets": [`${namespace}/app.ttl`] },
-		{ "regexp": ".*/partition/(\\w+)$", "targets": [`${namespace}/partitions.ttl`], "isLast": true  },
-		{ "regexp": ".*/account/(\\w+)$", "targets": [`${namespace}/accounts.ttl`,`${namespace}/account/$1.ttl`], "isLast": true },
-		{ "regexp": ".*/(credits|terms)$", "targets": [`${namespace}/$1.ttl`], "isLast": true },
+        { "regexp": ".*", "targets": [LODMAP2D_DATA + "app.ttl"] },
+        { "regexp": ".*/(table|partition|account).*$", "targets": [LODMAP2D_DATA + "accounts-index.ttl"] },
+        { "regexp": ".*/table$", "targets": [LODMAP2D_DATA + "table-view.ttl"], "isLast": true },
+        { "regexp": ".*/partition/(\\w+)$", "targets": [LODMAP2D_DATA + "partitions.ttl"], "isLast": true },
+        { "regexp": ".*/account/(\\w+)$", "targets": [LODMAP2D_DATA + "account-view.ttl", LODMAP2D_DATA + "account/$1.ttl"], "isLast": true },
+        { "regexp": ".*/(credits|terms)$", "targets": [LODMAP2D_DATA + "$1.ttl"], "isLast": true }
 	] ;
 }(this));
--- docker/run_server.sh
+++ docker/run_server.sh
@@ -9,10 +9,12 @@ if [ "${LODMAP2D_DATA:0:4}" = "http" ]; then
 		CONF
 	else
 		cat >> "$CONFIG_FILE" <<-CONF
-			{ "regexp": ".*", "targets": ['${LODMAP2D_DATA}app.ttl'] },
-			{ "regexp": ".*/partition/(\\\\w+)$", "targets": ["${LODMAP2D_DATA}partitions.ttl"], "isLast": true  },
-			{ "regexp": ".*/account/(\\\\w+)$", "targets": ["${LODMAP2D_DATA}accounts.ttl","${LODMAP2D_DATA}account/\$1.ttl"], "isLast": true },
-			{ "regexp": ".*/(credits|terms)$", "targets": ["${LODMAP2D_DATA}\$1.ttl"], "isLast": true }
+	        { "regexp": ".*", "targets": [LODMAP2D_DATA + "app.ttl"] },
+	        { "regexp": ".*/(table|partition|account).*$", "targets": [LODMAP2D_DATA + "accounts-index.ttl"] },
+	        { "regexp": ".*/table$", "targets": [LODMAP2D_DATA + "table-view.ttl"], "isLast": true },
+	        { "regexp": ".*/partition/(\\w+)$", "targets": [LODMAP2D_DATA + "partitions.ttl"], "isLast": true },
+	        { "regexp": ".*/account/(\\w+)$", "targets": [LODMAP2D_DATA + "account-view.ttl", LODMAP2D_DATA + "account/$1.ttl"], "isLast": true },
+	        { "regexp": ".*/(credits|terms)$", "targets": [LODMAP2D_DATA + "$1.ttl"], "isLast": true }
 		CONF
 	fi
 	echo "];}(this));" >> "$CONFIG_FILE"
